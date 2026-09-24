b0cb8dd5b0d9fd10a8a6c0ec0966d2b9037ae7d4
Deveadra <95103492+Deveadra@users.noreply.github.com>
2026-09-24 09:47:22 -0400

    Register brand preview model with Alembic

--- migrations/env.py
+++ migrations/env.py
@@ -5,6 +5,7 @@ from sqlalchemy import engine_from_config, pool
 
 from katcha import acquisition_models  # noqa: F401
 from katcha import brand_models  # noqa: F401
+from katcha import brand_preview_models  # noqa: F401
 from katcha import discovery_poll_models  # noqa: F401
 from katcha import discovery_trend_models  # noqa: F401
 from katcha import edit_blueprint_models  # noqa: F401
