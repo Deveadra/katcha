324b8a8a17ee5e6987b59a91269eb34c888263a1
Deveadra <95103492+Deveadra@users.noreply.github.com>
2026-09-24 06:35:53 -0400

    Register edit performance model with Alembic

--- migrations/env.py
+++ migrations/env.py
@@ -8,6 +8,7 @@ from katcha import brand_models  # noqa: F401
 from katcha import discovery_poll_models  # noqa: F401
 from katcha import discovery_trend_models  # noqa: F401
 from katcha import edit_blueprint_models  # noqa: F401
+from katcha import edit_performance_models  # noqa: F401
 from katcha import intelligence_models  # noqa: F401
 from katcha import longform_models  # noqa: F401
 from katcha import models  # noqa: F401
